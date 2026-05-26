8bfe289791b06a8b1417d03252246c71820f3fd3
Dedeiniel <d4niel.hernandez.contreras@gmail.com>
2026-05-26 12:49:46 -0400

    laser mejorado para reflejos y refracciones

---
+++ Assets/Scripts/LaserBeam.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam
+{
+    Vector3 pos, dir;
+    GameObject laserObj;
+    LineRenderer laser;
+    List<Vector3> laserIndices = new List<Vector3>();
+    public LaserBeam(Vector3 pos, Vector3 dir, Material material)
+    {
+        this.laser = new LineRenderer();
+        this.laserObj = new GameObject();
+        this.laserObj.name = "Laser Beam";
+        this.pos = pos;
+        this.dir = dir;
+
+        this.laser = this.laserObj.AddComponent(typeof(LineRenderer)) as LineRenderer;
+        this.laser.startWidth = 0.1f;
+        this.laser.endWidth = 0.1f;
+        this.laser.material = material;
+        this.laser.startColor = Color.white;
+        this.laser.endColor = Color.white;
+
+        CastRay(pos, dir, laser);
+    }
+
+    void CastRay(Vector3 pos, Vector3 dir, LineRenderer laser)
+    {
+        laserIndices.Add(pos);
+
+        Ray ray = new Ray(pos, dir); 
+        RaycastHit hit;
+        
+        if(Physics.Raycast(ray, out hit, 30,1))
+        {
+            laserIndices.Add(hit.point);
+            UpdateLaser();
+        }
+        else
+        {
+            laserIndices.Add(ray.GetPoint(30));
+            UpdateLaser();
+        }
+    }
+
+    void UpdateLaser()
+    {
+        int count = 0;
+        laser.positionCount = laserIndices.Count;
+
+        foreach (Vector3 idx in laserIndices)
+        {
+            laser.SetPosition(count, idx);
+            count++;
+        }
+    }
+}
---
+++ Assets/Scripts/ShootLaser.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ShootLaser : MonoBehaviour
+{
+    public Material material;
+    LaserBeam beam;
+
+    void Update()
+    {
+        Destroy(GameObject.Find("Laser Beam"));
+        beam = new LaserBeam(gameObject.transform.position, gameObject.transform.up, material);
+    }
+}
