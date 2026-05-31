f0dcec50eb4111d14f3ed9dae3870782b54bec44
Dedeiniel <d4niel.hernandez.contreras@gmail.com>
2026-05-31 15:37:20 -0400

    hub central funcional y transiciones

---
+++ Assets/Scripts/FakeLevelManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+
+public class FakeLevelManager : MonoBehaviour
+{
+    public static FakeLevelManager instance;
+
+    public CanvasGroup TransicionDeNivel;
+    public float TiempoTransicion = 1f;
+    [Space(5)]
+    public int JefesDerrotados;
+    [Tooltip("0 = UP, 1 = DOWN, 2 = RIGHT, 3 = LEFT")]
+    public Transform[] Levels;// 0 = UP, 1 = DOWN, 2 = RIGHT, 3 = LEFT
+    [Tooltip("0 = UP, 1 = DOWN, 2 = RIGHT, 3 = LEFT")]
+    public Transform[] PlayerSpawnPoints;// 0 = UP, 1 = DOWN, 2 = RIGHT, 3 = LEFT
+    [Space(5)]
+    public Transform Player;
+    public Transform MainCamera;
+
+    void Awake() 
+    {
+        instance = this;
+    }
+
+    public void ReturnToHub() 
+    {
+        FadeToBlack(TiempoTransicion);
+        Player.position = new Vector3(0f, 0f, 0f);
+        PlayerController.instance.activateRotation = true;
+        MainCamera.position = new Vector3(0f ,0f ,-10f);
+        FadeFromBlack(TiempoTransicion);
+    }
+
+    public void GoToLevel(float rotacion, int levelIndex) 
+    {
+        FadeToBlack(TiempoTransicion);
+        PlayerController.instance.activateRotation = false;
+        Player.rotation = Quaternion.Euler(0f, 0f, rotacion);
+        Player.position = PlayerSpawnPoints[levelIndex].position;
+        MainCamera.position = Levels[levelIndex].position;
+        FadeFromBlack(TiempoTransicion);
+    }
+
+    public void FadeToBlack(float duration) 
+    {
+        StartCoroutine(AnimateFade(0f, 1f, duration));
+    }
+
+    public void FadeFromBlack(float duration) 
+    {
+        StartCoroutine(AnimateFade(1f, 0f, duration));
+    }
+
+    private IEnumerator AnimateFade(float startAlpha, float targetAlpha, float duration) 
+    {
+        float timer = 0f;
+        TransicionDeNivel.alpha = startAlpha;
+
+        while (timer < duration) 
+        {
+            timer += Time.deltaTime;
+            TransicionDeNivel.alpha = Mathf.Lerp(startAlpha, targetAlpha, timer/duration);
+            yield return null;
+        }
+
+        TransicionDeNivel.alpha = targetAlpha;
+    }   
+
+}
---
+++ Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour
+{
+    [Tooltip("0 = UP, 1 = DOWN, 2 = RIGHT, 3 = LEFT")]
+    public int LevelIndex;// 0 = UP, 1 = DOWN, 2 = RIGHT, 3 = LEFT
+    [Space(5)]
+    [Tooltip("0f = UP, 180f = DOWN, -90 = RIGHT, 90 = LEFT")]
+    public float Rotation;// 0f = UP, 180f = DOWN, -90 = RIGHT, 90 = LEFT
+    [Space(5)]
+    public bool BossDefeated;
+    public GameObject Flecha;
+
+    private Collider thisCollider;
+
+    void Awake() 
+    {
+        thisCollider = GetComponent<Collider>();
+    }
+
+    void Update() 
+    {
+        if (BossDefeated) 
+        {
+            thisCollider.isTrigger = false;
+            Flecha.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Player")) 
+        {
+            FakeLevelManager.instance.GoToLevel(Rotation, LevelIndex);
+        }
+    } 
+
+}
